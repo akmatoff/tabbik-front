739c7e4d6c495439af24a6c0b272298c550463b2
akmatoff <akmatoff.exe@gmail.com>
2024-03-25 00:37:33 +0600

    Get userdata first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import RootRouter from "./components/RootRouter/RootRouter";
 import { Toaster } from "react-hot-toast";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useUserdata } from "./queries/userdata";
 
 function App() {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 60 * 1000,
-        refetchOnWindowFocus: false,
-        retry: false,
-      },
-    },
-  });
+  useUserdata();
 
   return (
     <>
       <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-        <QueryClientProvider client={queryClient}>
-          <Toaster />
-          <RootRouter />
-        </QueryClientProvider>
+        <Toaster />
+        <RootRouter />
       </GoogleOAuthProvider>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import '@/requests/request.ts';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@/requests/request.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
