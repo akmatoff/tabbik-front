72360b5ee06d7e752bf1dba330e013f91298d490
akmatoff <akmatoff.exe@gmail.com>
2024-03-28 01:49:36 +0600

    Profile page club icon

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -9,14 +9,20 @@ export default function Profile() {
 
   return (
     <div className="w-full min-h-screen">
-      <div className="bg-white p-6 rounded-primary">
+      <div className="bg-white p-6 rounded-primary flex flex-col">
         {userdata && user && club && !isLoading && (
           <>
             <h1 className="font-bold text-2xl">{`${user.first_name} ${user.last_name}`}</h1>
             <h1 className="font-bold">{user.username}</h1>
             <h1>{user.email}</h1>
-            <h1>{club.title}</h1>
-            <img src={club.icon} alt="club logo"></img>
+            <div className="flex items-center gap-4">
+              <img
+                className="w-20 h-20 rounded-secondary"
+                src={club.icon}
+                alt="club logo"
+              />
+              <h1>{club.title}</h1>
+            </div>
           </>
         )}
       </div>
